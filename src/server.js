67f212cca7897e1fe3a21e592c932fb9ff40f134
Matt Ravenmoore <m.ravenmoore@gmail.com>
2021-02-08 15:27:51 -0800

    trying to deploy

--- src/server.js
+++ src/server.js
@@ -13,6 +13,26 @@ const v1Routes = require('./routes/v1');
 
 const app = express();
 
+
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: 'http://localhost:3000',
+  clientID: '76AnvZ4HTAarvu39iYT13UicNgk5NotC',
+  issuerBaseURL: 'https://dev-uhg26xll.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
 //app level middleware
 app.use(cors());
 app.use(morgan('dev'));
