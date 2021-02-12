a3c2579d6324e5a5773ca9869a064b44384ecfea
Matt Ravenmoore <m.ravenmoore@gmail.com>
2021-02-12 12:39:17 -0800

    did the fix

--- src/server.js
+++ src/server.js
@@ -16,24 +16,13 @@ const jwtCheck = require('./auth/jwt-checker');
 
 const app = express();
 
-const { auth, requiresAuth } = require('express-openid-connect');
-
 const config = {
   authRequired: false,
   auth0Logout: true,
-  secret: process.env.SECRET,
   baseURL: process.env.BASE_URL,
-  clientID: '76AnvZ4HTAarvu39iYT13UicNgk5NotC',
   issuerBaseURL: 'https://dev-uhg26xll.us.auth0.com'
 };
 
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
-
-// req.isAuthenticated is provided from the auth router
-// app.get('/', (req, res) => {
-//   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-// });
 
 //app level middleware
 app.use(cors());
